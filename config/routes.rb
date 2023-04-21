3e366bf20dfa0536abc3826b3c7a93367b4f7ece
Emre Ozen <fossoyeuse@proton.me>
2023-04-20 20:48:14 -0400

    bookmarks controller needs work

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'lists', to: 'lists#index'
+  get 'lists/new', to: 'lists#new'
+  get 'lists/:id', to: 'lists#show', as: 'list'
+  post 'lists', to: 'lists#create'
+
+  get 'lists/:id/bookmarks/new', to: 'bookmarks#new', as: 'bookmark'
+  post 'lists/:id/bookmarks', to: 'bookmarks#create', as: 'list_bookmarks'
+  delete 'bookmarks/:id', to: 'bookmarks#destroy'
 end
